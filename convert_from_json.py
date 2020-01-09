a73d0dbd6c3edc1b38e8bafa93c356566205b937
Vyacheslav Gusev <vyacheslav.gusev@gmail.com>
2020-01-09 20:20:29 +0300

    tests rearragned

--- convert_from_json.py
+++ convert_from_json.py
@@ -5,18 +5,24 @@ import validators
 def to_url( record ):
     if 'disabled' in record:
         if not isinstance(record['disabled'], (bool)):
-            raise KeyError('"disabled" field must be boolean type')
+            raise ValueError('"disabled" field must be boolean type')
 
         if record['disabled']:
           return
 
+
     if not validators.domain(record['domain_name']):
-        raise KeyError('domain_name is in invalid format')
+        raise ValueError('domain_name is in invalid format')
+
     uri_builder = builder.URIBuilder().add_host(record['domain_name'])
 
+
     if 'username' in record:
-        #TODO: check if password and username is alphanumeric only
+        if not record['username'].isalnum():
+            raise ValueError('username must be alphanumeric string')
         if 'password' in record:
+            if not record['password'].isalnum():
+                raise ValueError('password must be alphanumeric string')
             uri_builder = uri_builder.add_credentials(record['username'],record['password'])
         else:
             uri_builder = uri_builder.add_credentials(record['username'])
@@ -24,17 +30,30 @@ def to_url( record ):
 
     if 'scheme' in record:
         if record['scheme'] not in ['http','https']:
-            raise KeyError('scheme is invalid, only http and https allowed')
+            raise ValueError('scheme is invalid, only http and https allowed')
         uri_builder = uri_builder.add_scheme(record['scheme'])
+
+
     if 'port' in record:
         uri_builder = uri_builder.add_port(record['port'])
+
     if 'path' in record:
         uri_builder = uri_builder.add_path(record['path'])
+        if not validators.url(uri_builder.finalize().unsplit()):
+            raise ValueError('path is invalid format')
+
     if 'query' in record:
         uri_builder = uri_builder.add_query_from(record['query'])
+        if not validators.url(uri_builder.finalize().unsplit()):
+            raise ValueError('query is invalid format')
+
     if 'fragment' in record:
         uri_builder = uri_builder.add_fragment(record['fragment'])
+        if not validators.url(uri_builder.finalize().unsplit()):
+            raise ValueError('fragment is invalid format')
        
+
     uri = uri_builder.finalize().unsplit()
+
 
     return uri
--- test_convert_from_json.py
+++ test_convert_from_json.py
@@ -6,33 +6,37 @@ import convert_from_json
 
 class TestConvertFromJSON(unittest.TestCase):
 
-    def test_required_field_is_absent(self):
-        self.assertRaises(KeyError, convert_from_json.to_url, {})
+    def test_all_args(self):
+        result = convert_from_json.to_url({"scheme": "http", "username":"user","password":"pass", "domain_name": "test.com", "port": "8080", "path": "path/to/file","fragment": "header2", "query": { "key1": "value1" } })
+        self.assertEqual(result, "http://user:pass@test.com:8080/path/to/file?key1=value1#header2")
 
-    def test_disabled_is_set(self):
+    def test_invalid_scheme(self):
+        self.assertRaises(ValueError, convert_from_json.to_url, {"domain_name":"test.com","scheme":"ftp"})
+
+    def test_invalid_domain(self):
+        self.assertRaises(KeyError, convert_from_json.to_url, {})
+        self.assertRaises(ValueError, convert_from_json.to_url, {"domain_name":"^"})
+
+    def test_disabled_field(self):
         result = convert_from_json.to_url({"disabled":True,"domain_name":"test.com"})
         self.assertEqual(result, None)
+        self.assertRaises(ValueError, convert_from_json.to_url, {"disabled":"not boolean","domain_name":"test.com"})
 
-    def test_disabled_is_not_set(self):
-        result = convert_from_json.to_url({"disabled":False,"domain_name":"test.com"})
-        self.assertEqual(result, "//test.com")
-
-    def test_disabled_is_not_boolean(self):
-        self.assertRaises(KeyError, convert_from_json.to_url, {"disabled":"not boolean","domain_name":"test.com"})
-
-    def test_valid_scheme(self):
-        result = convert_from_json.to_url({"domain_name":"test.com","scheme":"http"})
-        self.assertEqual(result, "http://test.com")
-
-    def test_scheme_is_invalid(self):
-        self.assertRaises(KeyError, convert_from_json.to_url, {"domain_name":"test.com","scheme":"ftp"})
-
-    def test_domain_is_invalid(self):
-        self.assertRaises(KeyError, convert_from_json.to_url, {"domain_name":"^"})
-
-    def test_port_is_not_integer(self):
+    def test_invalid_port(self):
         self.assertRaises(ValueError, convert_from_json.to_url, {"domain_name":"test.com","port":"not a port"})
+        self.assertRaises(ValueError, convert_from_json.to_url, {"domain_name":"test.com","port":"-123"})
+        self.assertRaises(ValueError, convert_from_json.to_url, {"domain_name":"test.com","port":"12.3"})
+        self.assertRaises(ValueError, convert_from_json.to_url, {"domain_name":"test.com","port":"66000"})
 
+    def test_invalid_path(self):
+        self.assertRaises(ValueError, convert_from_json.to_url, {"domain_name":"test.com","path":"^"})
+
+    def test_invalid_query(self):
+        self.assertRaises(TypeError, convert_from_json.to_url, {"domain_name":"test.com","query":"<>\^`{|}"})
+        self.assertRaises(ValueError, convert_from_json.to_url, {"domain_name":"test.com","query":{"key1":"<>\^`{|}"}})
+
+    def test_invalid_fragment(self):
+        self.assertRaises(ValueError, convert_from_json.to_url, {"domain_name":"test.com","fragment":"<>\^`{|}"})
 
 
 if __name__ == '__main__':
